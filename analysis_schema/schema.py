46a5e5179def7f33c9f0d4e6ba7a0fa762a4f74e
Joao Couto <jpcouto@gmail.com>
2025-08-18 22:16:17 -0700

    Removed channel_slice.

--- analysis_schema/schema.py
+++ analysis_schema/schema.py
@@ -170,7 +170,8 @@ class DredgeSpikeDetection(dj.Manual):
             rec = si.read_cbin_ibl(path)
             # restrict to channels with activity in this recording
             #rec = rec.channel_slice(rec.channel_ids[:300])
-            rec = rec.channel_slice(rec.channel_ids[:-1]) # remove sync
+            #rec = rec.channel_slice(rec.channel_ids[:-1]) # remove sync
+            # no need to remove the sync because it is not considered when using read_cbin_ibl
             rec = si.bandpass_filter(rec)
             rec = si.phase_shift(rec)
             rec = si.common_reference(rec)
